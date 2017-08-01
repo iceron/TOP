a2207dee705741f84851b3387168a60c3b1185c8
Enrico Lambino <enricolambino@yahoo.com>
2017-08-02 00:07:37 +0800

    added loop to be able to play again, comments

--- Ruby/oop/tic_tac_toe/tic_tac_toe.rb
+++ Ruby/oop/tic_tac_toe/tic_tac_toe.rb
@@ -1,16 +1,20 @@
 class Tic_tac_toe
+  #set player tokens
   @@player1 = "x"
   @@player2 = "o"
 
+  #initialization
   def initialize
     @data = ('0'...'9').to_a
     @player1_turn = true
   end
 
+  #check if value is of type int
   def integer?(value)
     Integer(value) != nil rescue false
   end
 
+  #show the board
   def display
     @out = ""
     @row = 0
@@ -29,10 +33,12 @@ class Tic_tac_toe
     end
   end
 
+  #check if a cell is already taken
   def taken?(choice)
     @data[choice] == @@player1 || @data[choice] == @@player2
   end
 
+  #update the data (board)
   def update(choice, input)
     if !taken?(choice)
       @data[choice] = input
@@ -41,24 +47,36 @@ class Tic_tac_toe
     return false
   end
 
-  def row_pattern(start, input)
+  #check for row pattern match
+  def row_pattern?(start, input)
     [@data[start], @data[start+1], @data[start+2]].all?{|word| word == input}
   end
-  def column_pattern(start, input)
+
+  #check for column pattern match
+  def column_pattern?(start, input)
     [@data[start], @data[start+3], @data[start+6]].all?{|word| word == input}
   end
-  def diagonal_pattern(start, input)
-    [@data[start], @data[start+4], @data[start+8]].all?{|word| word == input} ||
-    [@data[start], @data[start+2], @data[start+4]].all?{|word| word == input}
+
+  #check for diagonal pattern match
+  def diagonal_pattern?(start, input)
+    result = false
+    if start == 0
+      result = [@data[start], @data[start+4], @data[start+8]].all?{|word| word == input}
+    elsif start == 2
+      result = [@data[start], @data[start+2], @data[start+4]].all?{|word| word == input}
+    end
+    result
   end
 
+  #check for pattern match
   def pattern_match?(input)
-    row_pattern(0, input) || row_pattern(3, input) || row_pattern(6, input) ||
-    column_pattern(0, input) || column_pattern(1, input) || column_pattern(2, input) ||
-    diagonal_pattern(0, input) || diagonal_pattern(2, input)
+    row_pattern?(0, input) || row_pattern?(3, input) || row_pattern?(6, input) ||
+    column_pattern?(0, input) || column_pattern?(1, input) || column_pattern?(2, input) ||
+    diagonal_pattern?(0, input) || diagonal_pattern?(2, input)
   end
 
-  def player_win
+  #check if player won
+  def player_win?
     result = false
     if pattern_match?(@@player1)
       system "clear"
@@ -72,7 +90,8 @@ class Tic_tac_toe
     result
   end
 
-  def board_full
+  #check if board is full
+  def board_full?
     result = false
     if @data.all?{|item| item == @@player1 || item == @@player2}
       result = true
@@ -81,6 +100,7 @@ class Tic_tac_toe
     result
   end
 
+  #run a player's turn
   def player_turn
     system "clear"
     display
@@ -104,19 +124,36 @@ class Tic_tac_toe
     @player1_turn ^= true
   end
 
-  def run
-    puts "This is a game of tic-tac toe."
-    display
-    puts "Two players take turns in placing their tokens on the board."
-    puts "The cells on the board are numbered accordingly."
-    print "To begin, press enter."
-    input = gets.chomp
+  #play a game
+  def play
+
     while true
       player_turn
-      break if player_win || board_full
+      break if player_win? || board_full?
     end
     display
   end
+
+  #main sequence
+  def run
+    run = true
+    display
+    puts "This is a game of tic-tac toe."
+    puts "Two players take turns in placing their tokens on the board."
+    puts "The cells on the board are numbered accordingly."
+    print "To begin, press enter."
+    gets.chomp
+    begin
+      initialize
+      play
+      print "Play again (y/n)?: "
+      choice = gets.chomp
+      if (choice == "n" || choice == "N")
+        run = false
+      end
+    end until run == false
+  end
+
 end
 
 game = Tic_tac_toe.new
